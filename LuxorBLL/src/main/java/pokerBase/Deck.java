21dd502513189daf809591cfcb6975f93fb8d4bb
hmarnewe <hava@udel.edu>
2016-03-02 16:51:43 -0500

    Update Deck.java

--- LuxorBLL/src/main/java/pokerBase/Deck.java
+++ LuxorBLL/src/main/java/pokerBase/Deck.java
@@ -1,5 +1,32 @@
 package pokerBase;
 
-public class Deck {
+import pokerEnums.eRank;
+import pokerEnums.eSuit;
 
+public class Deck {
+  private ArrayList<Card> deckCards = new ArrayList<Card>();
+
+	/**
+	 * No arguments for deck, will return shuffled deck of 52 cards
+	 */
+	public Deck(){
+		//Create an ArrayList of Cards, add each card
+		for (short i = 0; i <= 3; i++) {
+			eSuit SuitValue = eSuit.values()[i];
+			for (short j = 0; j <= 12; j++) {
+				eRank RankValue = eRank.values()[j];
+				Card NewCard = new Card(SuitValue, RankValue, (13));
+				deckCards.add(NewCard);
+			}
+		}
+		Collections.shuffle(deckCards);
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public Card Draw()
+	{
+		return deckCards.remove(0);
+	}
 }
