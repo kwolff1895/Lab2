2d3eb5740e3302316967e4293ccd1dfc80ca0863
hmarnewe <hava@udel.edu>
2016-03-02 16:45:17 -0500

    Update eSuit.java

--- LuxorBLL/src/main/java/PokerEnums/eSuit.java
+++ LuxorBLL/src/main/java/PokerEnums/eSuit.java
@@ -1,5 +1,16 @@
 package PokerEnums;
 
 public enum eSuit {
+  HEARTS(1), SPADES(2), CLUBS(3), DIAMONDS(4);
+
+	private int iSuitNbr;
+
+	private eSuit(int iSuitNbr) {
+		this.iSuitNbr = iSuitNbr;
+	}
+
+	public int getiSuitNbr() {
+		return iSuitNbr;
+	}
 
 }
